e222a46234fb026e9c9dc625c56679089808fd3c
Christian Greiner <christian.greiner@posteo.net>
2023-06-24 10:52:16 +0200

    improve count_non_negative_numbers

--- divide-and-conquer/anzahlnichtnegativen.py
+++ divide-and-conquer/anzahlnichtnegativen.py
@@ -1,20 +1,20 @@
 
-def count_non_negative_numbers(arr, start, end):
-    if start > end:
+def count_non_negative_numbers(arr):
+    if len(arr) == 0:
         return 0
     
-    if start == end:
-        if arr[start] >= 0:
+    if len(arr) == 1:
+        if arr[0] >= 0:
             return 1
         else:
             return 0
     
-    mid = (start + end) // 2
-    count_left = count_non_negative_numbers(arr, start, mid)
-    count_right = count_non_negative_numbers(arr, mid+1, end)
+    mid = len(arr) // 2
+    count_left = count_non_negative_numbers(arr[:mid])
+    count_right = count_non_negative_numbers(arr[mid:])
 
     return count_left + count_right
 
 numbers = [2, -1, 0, 5, -3, 10, 8]
-result = count_non_negative_numbers(numbers, 0, len(numbers)-1)
+result = count_non_negative_numbers(numbers)
 print(result)
