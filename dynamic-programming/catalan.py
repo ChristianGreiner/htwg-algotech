dc1a100b2793106b2beb01c97af5f30293cab50d
Christian Greiner <christian.greiner@posteo.net>
2023-06-24 11:25:48 +0200

    fix catalan

--- dynamic-programming/catalan.py
+++ dynamic-programming/catalan.py
@@ -1,11 +1,12 @@
 def catalan(n: int = 0):
-    liste = [0] * (n + 1)
-    liste[0] = liste[1] = 1
+    c = [0] * (n + 1)
+    c[0] = c[1] = 1
 
-    for i in range(2, n + 1):
-        for j in range(i):
-            liste[i] += liste[j] * liste[i - j - 1]
+    for N in range(2, n + 1):
+        c[N] = 0
+        for k in range(N):
+            c[N] += c[k] * c[N - k - 1]
 
-    return liste[n]    
+    return c[n]
 
 print(catalan(5))
