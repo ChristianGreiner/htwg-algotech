81fc3f8e6cd148ab976464480d3534c75aad2f25
Christian Greiner <christian.greiner@posteo.net>
2023-06-24 22:48:36 +0200

    fix binom

--- dynamic-programming/binomialkoeffizient.py
+++ dynamic-programming/binomialkoeffizient.py
@@ -1,12 +1,11 @@
 def binom(n, k):
     komb = [0] * (n + 1)
-    for i in range(n):
-        komb[0] = 1
-        komb[i] = 1
+    komb[0] = 1
 
-        for k in range(i-1, 0, -1):
-            komb[k] = komb[k] + komb[k-1]
+    for i in range(1, n + 1):
+        for j in range(i, 0, -1):
+            komb[j] = komb[j] + komb[j-1]
 
     return komb[k]
 
-print(binom(5, 9))
+print(binom(9, 5))
